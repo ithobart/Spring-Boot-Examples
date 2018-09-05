afa9336fe9f0bc996c680e60301a547f87f32021
1479228740@qq.com <1479228740@qq.com>
2018-09-05 14:46:52 +0800

    问题修改

--- springboot-mail/src/test/java/com/ithobart/mail/mailTest.java
+++ springboot-mail/src/test/java/com/ithobart/mail/mailTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class mailTest{
+public class MailTest {
 
     @Autowired
     private MailService mailService;
