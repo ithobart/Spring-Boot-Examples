15b0d2eba70badb838d19fca8cbfe37dcadc9899
1479228740@qq.com <1479228740@qq.com>
2018-09-06 10:18:25 +0800

    定时任务初始化

---
+++ springboot-schedu/src/main/java/com/ithobart/schedu/ScheduApplication.java
@@ -0,0 +1,12 @@
+package com.ithobart.schedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduApplication.class, args);
+	}
+}
